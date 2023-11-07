c79256ab138f502e6030dd7ff23293d2e2d57273
Nel <nel.renaudin@gmail.com>
2023-11-07 10:20:50 +0100

    Connect to database

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require("express")
 require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+
+// Connect to database
+const mongoDB = process.env.MONGODB_URI
+mongoose.set("strictQuery", false)
+mongoose.connect(mongoDB).catch((err) => console.error(err))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
